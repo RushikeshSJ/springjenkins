125885bdec8a0ce1b902eb13ca9a26c47dfeb56b
Rushikesh Jadhav <Rushikeshjadhav2018@gmail.com>
2023-07-20 13:04:39 +0530

    Second

--- src/main/java/com/jenkins/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/jenkins/springjenkins/SpringJenkinsApplication.java
@@ -8,6 +8,7 @@ public class SpringJenkinsApplication {
 
     public static void main(String[] args) {
         System.out.println("Project Started");
+        System.out.println("Hello");
         SpringApplication.run(SpringJenkinsApplication.class, args);
     }
 
